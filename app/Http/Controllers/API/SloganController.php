7d8a05596866cabf19a366f358adf3566fd535d5
Mahdi Kazemy <m4362746@gmail.com>
2024-02-28 11:18:04 +0330

    create strucher for SloganController and TargetController

--- app/Http/Controllers/API/SloganController.php
+++ app/Http/Controllers/API/SloganController.php
@@ -9,57 +9,36 @@ use App\Models\slogan;
 
 class SloganController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreSloganRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(slogan $slogan)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(slogan $slogan)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateSloganRequest $request, slogan $slogan)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(slogan $slogan)
     {
         //
--- app/Http/Controllers/API/TargetController.php
+++ app/Http/Controllers/API/TargetController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class TargetController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store( $request)
+    {
+        //
+    }
+
+    public function show( $slogan)
+    {
+        //
+    }
+
+    public function edit( $slogan)
+    {
+        //
+    }
+
+    public function update( $request,  $slogan)
+    {
+        //
+    }
+
+    public function destroy( $slogan)
+    {
+        //
+    }
 }
