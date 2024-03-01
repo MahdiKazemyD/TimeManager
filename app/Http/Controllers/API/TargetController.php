540809ffb9b0b7a216f7094fbfe0d7f6d1c344ed
Mahdi Kazemy <m4362746@gmail.com>
2024-03-01 22:42:49 +0330

    create struchor for Target Contoller and Service

--- app/Http/Controllers/API/TargetController.php
+++ app/Http/Controllers/API/TargetController.php
@@ -3,23 +3,28 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Target\StoreTargetRequest;
+use App\Repositories\Target\TargetRepositoryInterface;
+use App\Service\TargetService;
 use Illuminate\Http\Request;
 
 class TargetController extends Controller
 {
+    public function __construct(
+        readonly protected TargetRepositoryInterface $targetRepository,
+        readonly protected TargetService $targetService
+    )
+    {
+    }
+
     public function index()
     {
-        //
+        return $this->targetRepository->allForUser();
     }
 
-    public function create()
+    public function store(StoreTargetRequest $request)
     {
-        //
-    }
-
-    public function store( $request)
-    {
-        //
+        return $this->targetService->store($request);
     }
 
     public function show( $slogan)
--- app/Service/TargetService.php
+++ app/Service/TargetService.php
@@ -2,7 +2,24 @@
 
 namespace App\Service;
 
+use App\Http\Requests\Target\StoreTargetRequest;
+use App\Http\Requests\Target\UpdateTargetRequest;
+use App\Repositories\Target\TargetRepositoryInterface;
+
 class TargetService
 {
+    public function __construct(readonly protected TargetRepositoryInterface $targetRepository)
+    {
+    }
+
+    public function store(StoreTargetRequest $request)
+    {
+        return $this->targetRepository->create($request->toArray());
+    }
+
+    public function update(int $id, UpdateTargetRequest $request)
+    {
+        return $this->targetRepository->update($id, $request->toArray());
+    }
 
 }
