a0bcfb7f67e25fa0cbbd6a063a6d270be3288d3d
Mahdi Kazemy <m4362746@gmail.com>
2024-03-03 18:23:03 +0330

    create struchor CRUD for Task

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\StoreTaskRequest;
+use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Repositories\Task\TaskRepositoryInterface;
+use App\Service\TaskService;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function __construct(
+        readonly protected TaskRepositoryInterface $taskRepository,
+        readonly protected TaskService $taskService
+    )
+    {
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        //
+    }
+    public function update(int $id, UpdateTaskRequest $request)
+    {
+        //
+    }
+
+    public function destroy(int $id)
+    {
+        //
+    }
 }
