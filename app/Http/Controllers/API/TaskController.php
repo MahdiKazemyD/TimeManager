5899b2b2b4efe57903f08982ba8620debb5b29f9
Mahdi Kazemy <m4362746@gmail.com>
2024-03-04 13:11:07 +0330

    CRUD for Task with service

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -20,20 +20,20 @@ class TaskController extends Controller
 
     public function index()
     {
-        //
+        return $this->taskRepository->allForUser();
     }
 
     public function store(StoreTaskRequest $request)
     {
-        //
+        return $this->taskService->store($request);
     }
     public function update(int $id, UpdateTaskRequest $request)
     {
-        //
+        return $this->taskService->update($id, $request);
     }
 
     public function destroy(int $id)
     {
-        //
+        return $this->taskRepository->delete($id);
     }
 }
--- app/Service/TaskService.php
+++ app/Service/TaskService.php
@@ -2,7 +2,23 @@
 
 namespace App\Service;
 
+use App\Http\Requests\Task\StoreTaskRequest;
+use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Repositories\Task\TaskRepositoryInterface;
+
 class TaskService
 {
+    public function __construct(readonly protected TaskRepositoryInterface $taskRepository)
+    {
+    }
 
+    public function store(StoreTaskRequest $request)
+    {
+        return $this->taskRepository->create($request->toArray());
+    }
+
+    public function update(int $id, UpdateTaskRequest $request)
+    {
+        return $this->taskRepository->update($id, $request->toArray());
+    }
 }
