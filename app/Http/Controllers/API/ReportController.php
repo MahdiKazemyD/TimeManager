77756ef255ab4d320051933b730cbd481d4bf6fc
Mahdi Kazemy <m4362746@gmail.com>
2024-03-05 11:44:52 +0330

    create struchor Report Controller and Service

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Report\StoreReportRequest;
 use App\Http\Requests\Report\UpdateReportRequest;
+use App\Repositories\Report\ReportRepositoryInterface;
+use App\Service\ReportService;
 
 class ReportController extends Controller
 {
+    public function __construct(
+        readonly protected ReportRepositoryInterface $repository,
+        readonly protected ReportService $service
+    )
+    {
+    }
+
     public function index()
     {
-        //
+        return $this->repository->allForUser();
     }
 
     public function store(StoreReportRequest $request)
--- app/Service/ReportService.php
+++ app/Service/ReportService.php
@@ -2,7 +2,24 @@
 
 namespace App\Service;
 
+use App\Http\Requests\Report\StoreReportRequest;
+use App\Http\Requests\Report\UpdateReportRequest;
+use App\Repositories\Report\ReportRepositoryInterface;
+
 class ReportService
 {
+    public function __construct(readonly protected ReportRepositoryInterface $repository)
+    {
+    }
+
+    public function store(StoreReportRequest $request)
+    {
+        return $this->repository->create($request->toArray());
+    }
+
+    public function update(int $id, UpdateReportRequest $request)
+    {
+        return $this->repository->update($id, $request->toArray());
+    }
 
 }
